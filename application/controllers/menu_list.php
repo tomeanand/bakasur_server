ce0716dab8c050f4f5d2f742d482c19564b3181e
Anand Vimalanathan <anand@picsean.com>
2014-06-17 15:47:54 +0530

    rest api added

--- application/controllers/menu_list.php
+++ application/controllers/menu_list.php
@@ -68,39 +68,6 @@ class Menu_list extends CI_Controller {
   }
 
 
-  public function get_all_menu_list(){
-
-    $this->load->model('menu_list_model');
-    $this->load->model('menu_image_model');
-    $menu_query = $this->menu_list_model->get_all_menu();
-    if($menu_query->num_rows() > 0)  {
-      foreach($menu_query->result() as $menu_row){ 
-        $menu_id = $menu_row->menu_id;
-        $image_query = $this->menu_image_model->get_all_image($menu_id);
-        $image_array = array();
-        if($image_query->num_rows > 0)  {
-            foreach($image_query->result() as $image_row) {
-                array_push($image_array,$image_row->image_name);
-            }
-        }
-
-        $menu_list = (count($image_array) > 0 ?  implode(',',$image_array) : $menu_row->menu_image);
-        $menu_result[] = array("menu_id" => $menu_id,
-           "menu_name" => $menu_row->menu_name,
-           "menu_image" => $menu_list,
-           "menu_category" => $menu_row->menu_category,
-           "menu_cuisine" => $menu_row->menu_cuisine,
-           "menu_subcategory" => $menu_row->menu_subcategory,
-           "menu_description" => $menu_row->menu_description,
-           "menu_servefor" => $menu_row->menu_servefor,
-           "menu_price" => $menu_row->menu_price,
-
-           );
-      }
-      echo json_encode($menu_result);
-
-    }
-  }  
 
 
 //@class ends
