d8e70ab0976ae8c6054afd00480b1b050c4512e1
Anand Vimalanathan <anand@picsean.com>
2014-06-17 11:10:46 +0530

    image in rest made as array

--- application/views/template.php
+++ application/views/template.php
@@ -64,7 +64,7 @@
   <div class="container bs-docs-container">
     <div class="row">
       <div class="col-md-1" role="main"></div>
-      <div class="col-md-9" role="main">
+      <div class="col-md-10" role="main">
       
       <?php $this->load->view($content);?>
       </div>
