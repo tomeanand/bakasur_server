a0baa7b6efe73ca4b74a3e770b8a677553c1eb1a
Anand V <tomeanand@gmail.com>
2014-11-15 01:28:22 +0530

    updated user login

---
+++ application/models/customer_model.php
@@ -0,0 +1,35 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Customer_model extends CI_Model{
+    private $__table = 'cm_customer';
+    
+    
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function add_user($data){
+        $query = $this->db->insert($this->__table, $data); 
+        return $query;
+    }
+
+     public function has_user_registered($email){
+        $this->db->where('customer_email',$email);
+        $result = $this->db->get($this->__table);
+        return $result->num_rows();     	
+     }
+
+     
+     public function get_user($email){
+        $this->db->where('customer_email',$email);
+        return $this->db->get($this->__table);          
+     }
+
+     public function update_user($email,$update_row)    {
+        $this->db->where('customer_email',$email);
+        return $this->db->update($this->__table, $update_row);   
+    }           
+
+    
+}	
